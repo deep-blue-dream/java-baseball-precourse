33cd14d161020e14cf96578b28b44075c9ec4e6b
tutleCord <102152693+deep-blue-dream@users.noreply.github.com>
2022-10-10 14:10:58 +0900

    FEAT:게임진행자Owner생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,27 @@
 package baseball;
+import baseball.model.Owner;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
+        //게임 진행자 Owner 생성
+        Owner owner = new Owner();
+        //오너와 함께 게임을 하자.
+        gameStart(owner);
+    }
+
+    //게임 메서드 생성
+    private static void gameStart(Owner owner){
 
         boolean gameJudge = true;
 
         while (gameJudge == true) {
-            //게임 진행내용 적을 예정.
+                //게임 진행내용 적을 예정.
 
-            //게임 종료
-            break;
+                //게임 종료
+                break;
         }
     }
+
 }
